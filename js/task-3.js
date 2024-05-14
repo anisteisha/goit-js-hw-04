82a3f8f0d0f8eab95c087b94f531cf29a9d6690d
anisteisha <lereenaarakurt@gmail.com>
2024-05-14 14:38:24 +0100

    GH4B

---
+++ js/task-3.js
@@ -0,0 +1,18 @@
+function filterArray(numbers, value) {
+   const filterArray = [];
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > value) {
+            filterArray.push(numbers[index]);
+        }
+
+    }
+                        return filterArray;
+
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
